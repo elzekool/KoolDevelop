5e33f439527497d6f61ed83de6311419dbb0d9e7
Elze Kool <info@kooldevelopment.nl>
2012-08-23 16:40:06 +0200

    IArrayConverter interface added - Simple Model <-> Array converter interface added - SimpleArrayConverter class added

---
+++ framework/model/IArrayConverter.php
@@ -0,0 +1,50 @@
+<?php
+/**
+ * Array <-> Model Converter Interface
+ *
+ * @author Elze Kool
+ * @copyright Elze Kool, Kool Software en Webdevelopment
+ *
+ * @package KoolDevelop
+ * @subpackage Model
+ **/
+
+namespace KoolDevelop\Model;
+
+/**
+ * Array <-> Model Converter Interface
+ *
+ * Interface for Array <-> Model Conversion. Classes implementing this interface
+ * support the conversion of model data to a simple array. Use this interface
+ * for RESTfull controller actions and saving/retrieving form data.
+ *
+ * @author Elze Kool
+ * @copyright Elze Kool, Kool Software en Webdevelopment
+ *
+ * @package KoolDevelop
+ * @subpackage Model
+ **/
+interface IArrayConverter
+{
+    /**
+     * Convert Model data to array
+     * 
+     * @param \Model $model Model
+     * 
+     * @return mixed[] Array
+     */
+    public function convertModelToArray(\Model &$model);
+    
+    
+    /**
+     * Convert Array to Model data
+     * 
+     * @param mixed[] $data  Input Arrat
+     * @param \Model  $model Model
+     * 
+     * @return mixed[] Array
+     */
+    public function convertArrayToModel($data, \Model &$model);
+    
+    
+}
---
+++ framework/model/SimpleArrayConverter.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * Simple Model <-> Array converter
+ *
+ * @author Elze Kool
+ * @copyright Elze Kool, Kool Software en Webdevelopment
+ *
+ * @package KoolDevelop
+ * @subpackage Model
+ **/
+
+namespace KoolDevelop\Model;
+
+/**
+ * Simple Model <-> Array converter
+ *
+ * @author Elze Kool
+ * @copyright Elze Kool, Kool Software en Webdevelopment
+ *
+ * @package KoolDevelop
+ * @subpackage Model
+ **/
+class SimpleArrayConverter implements \KoolDevelop\Model\IArrayConverter
+{
+    
+    
+    public function convertArrayToModel($data, \Model &$model) {
+        
+    }
+    
+    public function convertModelToArray(\Model &$model) {
+        
+    }
+}
